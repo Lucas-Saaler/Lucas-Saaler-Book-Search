2484d565b0511860cba068491b1280b9e1b6cffd
Lucas Saaler <lucassaaler00@gmail.com>
2022-10-07 10:26:03 -0400

    Fixed saved book view page

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -8,10 +8,26 @@ import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
-  const { loading, data } = useQuery( GET_ME );
-  console.log(data)
+
+  
+
   // use this to determine if `useEffect()` hook needs to run again
+  const { loading, data } = useQuery( GET_ME );
+
   const userDataLength = Object.keys(userData).length;
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+          console.log(data.me)
+          setUserData(data.me);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getUserData();
+  }, [data]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
